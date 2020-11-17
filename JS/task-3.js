d68a4af68251c322c03cd7a0ab1a7a3aa63f4242
Valeriy Voronets <Voronets.v@gmail.com>
2020-11-17 13:30:09 +0200

    task-4

---
+++ JS/task-3.js
@@ -0,0 +1,58 @@
+class Storage {
+    constructor(items) {
+        this.items = items;
+    }
+    getItems() {
+        return this.items;
+    }
+
+    addItem(item) {
+        this.items.push(item)
+    }
+
+    removeItem(item) {
+    
+        this.items.splice(this.items.indexOf(item), 1);
+                }
+           
+}  
+
+console.log(typeof Storage);
+// 'function'
+
+const goods = [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+];
+
+const storage = new Storage(goods);
+
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+] */
+
+storage.addItem('Дроид');
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+
+storage.removeItem('Пролонгер');
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+
